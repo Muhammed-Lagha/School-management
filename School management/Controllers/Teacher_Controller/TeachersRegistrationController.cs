9ce49a60b2d0989a1fe3963ec136d6aa86327fdd
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-05-30 22:26:01 +0200

    Add Teacher Registration Controller

---
+++ School management/Controllers/Teacher_Controller/TeachersRegistrationController.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Exchange.WebServices.Data;
+using Org.BouncyCastle.Crypto.Generators;
+using School_management.DTOs;
+using School_management.DTOs.RequestDTOs;
+using School_management.Methods;
+using School_management.Models;
+using School_management.Services;
+
+namespace School_management.Controllers.Teacher_Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeachersRegistrationController : ControllerBase
+    {
+        public static Teacher teaher = new Teacher();
+
+        [HttpPost("register")]
+        public async Task<ActionResult<Teacher>> Register([FromBody] Teacher teacher)
+        {
+            try
+            {
+               
+            }
+            catch (Exception ex) { }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<ServiceResponse<Teacher>>> Login([FromBody] LoginTeacherRequests loginTeacherRequests)
+        {
+            try 
+            {
+                var serviceResponseTeacher = await RegistrationTeacherServices.LoginTeacherService(loginTeacherRequests.NiNo, loginTeacherRequests.Password);
+                return Ok(serviceResponseTeacher);
+            }
+            catch (Exception ex) 
+            {
+                var serviceResponse = new ServiceResponse<Teacher>(null!, false, ex.Message, null!);
+                return BadRequest(serviceResponse);
+            }
+        }
+    }
+}
