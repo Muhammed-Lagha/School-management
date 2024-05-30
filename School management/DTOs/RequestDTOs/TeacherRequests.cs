08dcc73534973deb90513752d8ac72d5fa311b8a
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-05-30 22:07:18 +0200

    Add Request DTOs and Teacher DTOs Files

---
+++ School management/DTOs/RequestDTOs/TeacherRequests.cs
@@ -0,0 +1,8 @@
+﻿namespace School_management.DTOs.RequestDTOs
+{
+    public class LoginTeacherRequests
+    {
+        public string NiNo { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
