f40d9e394283b93972f68f69e04adae82c0ebfed
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-05-30 22:08:24 +0200

    Add Teacher Repo

---
+++ School management/Repositories/Teacher_Repository/TeacherRepository.cs
@@ -0,0 +1,20 @@
+﻿using School_management.Models;
+
+namespace School_management.Repositories.Teacher_Repository
+{
+    public class TeacherRepository
+    {
+        public static async Task<Teacher> GetTeacherByNiNo(string NiNo)
+        {
+            var db = new SchoolManagementContext();
+            var teacher = await db.Teachers.Where(x => x.NiNo == NiNo);
+
+            if (teacher == null)
+            {
+                throw new Exception("Invalid NiNo or Password");
+            }
+
+            return teacher;
+        }
+    }
+}
