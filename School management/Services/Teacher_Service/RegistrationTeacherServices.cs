84fd3025448e8194bde5b90a9acf833ecce29114
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-05-30 22:09:59 +0200

    Add Registration Services to Teacher

---
+++ School management/Services/Teacher_Service/RegistrationTeacherServices.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using School_management.DTOs;
+using School_management.Methods;
+using School_management.Models;
+using School_management.Repositories.Teacher_Repository;
+
+namespace School_management.Services
+{
+    public class RegistrationTeacherServices
+    {
+        public static async Task<ServiceResponse<Teacher>> LoginTeacherService(string NiNo , string Password) 
+        {
+            Teacher teacher = await TeacherRepository.GetTeacherByNiNo(NiNo);
+            await AccessMethods.VerifyPassword(teacher ,Password);
+
+            teacher.Password = string.Empty;
+            var serviceResponse = new ServiceResponse<Teacher>(teacher, true, "");
+
+            return serviceResponse;
+        }
+    }
+}
