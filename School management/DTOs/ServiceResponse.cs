30c87ed55f35783f8283c63f2f52c480fab571b4
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-05-30 22:11:15 +0200

    Add Service Response DTO file

---
+++ School management/DTOs/ServiceResponse.cs
@@ -0,0 +1,36 @@
+﻿namespace School_management.DTOs
+{
+    public class ServiceResponse<T>
+    {
+
+        public ServiceResponse(T data, bool success, string message)
+        {
+            Data = data;
+            Success = success;
+            Message = message;
+        }
+
+        public ServiceResponse(T data, bool success, string message, string jwt)
+        {
+            Data = data;
+            Success = success;
+            Message = message;
+            //Jwt = jwt;
+        }
+
+        public ServiceResponse(T data, bool success, string message, string jwt, string refreshToken)
+        {
+            Data = data;
+            Success = success;
+            Message = message;
+            //Jwt = jwt;
+           // RefreshToken = refreshToken;
+        }
+
+        public T? Data { get; set; }
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = string.Empty;
+        //public string? Jwt { get; set; }
+        //public string? RefreshToken { get; set; }
+    }
+}
