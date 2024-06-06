9e503a0d72b892e2f394dae0fe02165561736673
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-06-06 22:53:26 +0200

    Add JWT Authentication

---
+++ School management/Controllers/APIsTesterController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using School_management.Models;
+
+namespace School_management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIsTesterController : ControllerBase
+    {
+
+        //  Bearer 
+        //  172737737
+        [HttpPost("GetAllTeachers")]
+        [Authorize()]
+        public async Task<ActionResult<List<Teacher>>> GetAllTeachers()
+        {
+            try
+            {
+                var db = new SchoolManagementContext();
+
+                List<Teacher> teacher = await db.Teachers.ToListAsync();
+                return Ok(teacher);
+            }
+            catch (Exception ex)
+            {
+               
+               return BadRequest(ex);
+            }
+        }
+
+        [HttpPost("GetAllStudents")]
+        [Authorize()]
+        public async Task<ActionResult<List<Student>>> GetAllStudents()
+        {
+            try
+            {
+                var db = new SchoolManagementContext();
+
+                List<Student> students = await db.Students.ToListAsync();
+                return Ok(students);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ School management/Methods/Tokens.cs
@@ -0,0 +1,84 @@
+﻿using School_management.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace DeviseHR_Server.Common
+{
+    public static class Tokens
+    {
+        public static async Task<string> GenerateTeacherAuthJWT(Teacher teacher)
+        {
+            //string ddd = user.Company.AnnualLeaveStartDate.ToString()
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("jwtSecrDD6huyukhjhjddddfdhjhjhjhhjhh#$3$3435343hhfhfh43434345465465465464645645645646545djddhjdhjdfdhet"));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var claims = GenerateTeacherJwtClaims(teacher);
+
+            var token = new JwtSecurityToken(
+                expires: DateTime.UtcNow.AddMinutes(1600),
+                signingCredentials: credentials,
+                claims: claims // Add the claims to the token
+            );
+
+            var jwt = await Task.Run(() => new JwtSecurityTokenHandler().WriteToken(token));
+
+            return jwt;
+        }
+
+        public static List<Claim> GenerateTeacherJwtClaims(Teacher teacher)
+        {
+            var claims = new List<Claim>();
+
+            
+                claims = new List<Claim>
+                {
+                    new Claim("id", teacher.Id.ToString()),
+                    new Claim("userName", teacher.Username.ToString()),
+                };
+            
+           
+
+            return claims;
+        }
+
+        public static async Task<string> GenerateStudentAuthJWT(Student student)
+        {
+            //string ddd = user.Company.AnnualLeaveStartDate.ToString()
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("jwtSecrDD6huyukhjhjddddfdhjhjhjhhjhh#$3$3435343hhfhfh43434345465465465464645645645646545djddhjdhjdfdhet"));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var claims = GenerateStudentJwtClaims(student);
+
+            var token = new JwtSecurityToken(
+                expires: DateTime.UtcNow.AddMinutes(1600),
+                signingCredentials: credentials,
+                claims: claims // Add the claims to the token
+            );
+
+            var jwt = await Task.Run(() => new JwtSecurityTokenHandler().WriteToken(token));
+
+            return jwt;
+        }
+
+        public static List<Claim> GenerateStudentJwtClaims(Student student)
+        {
+            var claims = new List<Claim>();
+
+
+            claims = new List<Claim>
+                {
+                    new Claim("id", student.StudentId.ToString()),
+                    new Claim("userName", student.Username.ToString()),
+                };
+
+
+
+            return claims;
+        }
+    }
+}
