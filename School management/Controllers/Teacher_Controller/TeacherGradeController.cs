338f25b7bb8303a06bbb6986c5ea7a2da3e5380d
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-06-12 19:33:45 +0200

    add Create Grade EndPoint

---
+++ School management/Controllers/Teacher_Controller/TeacherGradeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Exchange.WebServices.Data;
+using School_management.DTOs;
+using School_management.DTOs.RequestDTOs;
+using School_management.Models;
+
+namespace School_management.Controllers.Teacher_Controller
+{
+    [Route("api/[controller]")]
+    [ApiController , Authorize]
+    
+    public class TeacherGradeController : ControllerBase
+    {
+        [HttpPost("CreateGrade") , Authorize(Roles = "Teacher")]
+        public ActionResult<ServiceResponse<Grade>> CreateGrade(CreateGradeRequests gradeRequests)
+        {
+            try
+            {
+                var db = new SchoolManagementContext();
+
+                Grade grade = new Grade {
+                    GradeNumber = gradeRequests.GradeNumber,
+                    Name = gradeRequests.GradeName
+                };
+
+                db.Add(grade);
+                db.SaveChanges();
+
+                var serviceResponse = new ServiceResponse<Grade>(grade, true, "");
+                return Ok(serviceResponse);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ School management/DTOs/RequestDTOs/CreateGradeRequests.cs
@@ -0,0 +1,9 @@
+﻿namespace School_management.DTOs.RequestDTOs
+{
+    public class CreateGradeRequests
+    {
+        public string GradeName { get; set; } = string.Empty;
+
+        public int GradeNumber { get; set; }
+    }
+}
